53ba4361158e7f2e370b643b3279d94f2ca07fde
Martin Nuc <martin@nuc.cz>
2019-02-05 00:54:35 +0100

    prepare playground

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Observable,
+  from,
+  interval,
+  forkJoin,
+  merge,
+  combineLatest,
+  timer,
+  Subject
+} from 'rxjs';
+import {
+  filter,
+  reduce,
+  take,
+  skip,
+  map,
+  switchMap,
+  tap,
+  shareReplay
+} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'playground';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {
+  }
+
+  /**
+   * here is your playground
+   */
+  ngOnInit() {
+
+  }
+
+  /********** wait for subjects ********* */
+  fetchJoke() {
+    return this.http.get('https://api.chucknorris.io/jokes/random');
+  }
 }
